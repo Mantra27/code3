4dd45f4ff766addd3309248e5767f914c532789b
Mantra <75286433+Mantra27@users.noreply.github.com>
2021-08-30 18:00:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
-const express = require('express');
-const {DeclareFiles} = require('./DeclareFiles.js')
-const {execCPP} = require('./execCPP.js')
-const {execJS} = require('./execJS.js')
-//generateFile(extension, code)
+const { DeclareFiles } = require("./DeclareFiles.js");
+const { execJS } = require("./exec_modules/execJS.js");
+const { execCPP } = require("./exec_modules/execCPP.js");
+const { execPY } = require("./exec_modules/execPY.js");
+const { execCH } = require("./exec_modules/execCH.js");
+const { execC } = require("./exec_modules/execC.js");
+const cors = require('cors')
+
+const express = require("express");
 const app = express();
 const PORT = 6969;
 
-app.listen(PORT, ()=>{
-    console.log(`APP's Online! at port ${PORT}`)
-})
-
-app.get('/', (req, res)=>{
-    return res.json({hello: 'world'});
+app.use(cors())
+app.listen(PORT, () => {
+  console.log(`APP's Online! at port ${PORT}`);
 });
 
-app.post(`/run`, async (req, res)=>{
-    const {language = 'default', code} = req.query;
-    if(code === undefined || code == "" || code === null){
-        return res.status(400).json({err: 'empty code'})
-    }
-    
-    else{
-        try{
-        const filePath = await DeclareFiles(language, code)
-        // const output = await execCPP(filePath)
-        const output = await execJS(filePath)
+app.get("/", (req, res) => {
+  return res.json({ server: "running" });
+});
 
-        return res.json({output});
-        // return res.json({language, code})
-        }
-        catch(err){
-            res.status(500).json({err});
-            console.log(err)
-        }
+app.post(`/run`, async (req, res) => {
+  const { language = "default", code } = req.query;
+  if (code === undefined || code == "" || code === null) {
+    return res.status(400).json({ err: "empty code" });
+  } else {
+    try {
+      const filePath = await DeclareFiles(language, code);
+
+      let fileExtention = filePath.split(".")[1];
+      console.log(fileExtention)
+      if(fileExtention == 'js'){
+        let output = await execJS(filePath);
+        return res.json({ output });
+      }
+      else if(fileExtention == 'cpp'){
+        let output = await execCPP(filePath);
+        return res.json({ output });
+      }
+      else if(fileExtention == 'py'){
+        let output = await execPY(filePath);
+        return res.json({ output });
+      }
+      else if(fileExtention == 'ch'){
+        let output = await execCH(filePath);
+        return res.json({ output });
+      }
+      else if(fileExtention == 'c'){
+        let output = await execC(filePath);
+        return res.json({ output });
+      }
+
+      
+      // return res.json({language, code})
+    } catch (err) {
+      res.status(500).json({ err });
+      console.log(err);
     }
-})
+  }
+});
