ce7536da8d7a9b4e4e817aa4dffcda8ebada6bf6
Mantra <75286433+Mantra27@users.noreply.github.com>
2021-09-02 08:40:49 +0530

    Update execCPP.js

--- exec_modules/execCPP.js
+++ exec_modules/execCPP.js
@@ -1,8 +1,8 @@
 const {exec} = require('child_process');
-const execCPP = (input) =>{
-    console.log(`cd code && g++ ${input} -o ${input}.exe && ${input}.exe`)
+const execJS = (input) =>{
+    console.log(`cd code && node ${input}`)
     return new Promise((resolve, reject)=>{
-         exec(`cd code && g++ ${input}.cpp -o ${input}.exe && ./${input}.exe`, (error, stdout, stderr)=>{
+        exec(`cd code && node ${input}.js`, (error, stdout, stderr)=>{
             if(error) {
                 reject({error, stderr});
             }
@@ -15,5 +15,5 @@ const execCPP = (input) =>{
     }
     
 module.exports = {
-    execCPP,
+    execJS,
 };
